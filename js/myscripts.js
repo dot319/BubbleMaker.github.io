ad09e3905f62e83b2e4a0e7d508c3651e2c8a5d3
Dorieke Berends <dorieke90@gmail.com>
2018-10-13 16:38:42 +0200

    You can now make bubbles by right clicking

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,99 +1,138 @@
-window.addEventListener('resize', resizeCanvas);
-window.addEventListener('mousedown', deleteBubble);
-
 function letsStart() {
-    myCanvas = document.getElementById("canvas");
-    myOverlay = document.getElementById("overlay");
-    myFlexboxControls = document.getElementById("flexbox-controls");
-    myFlexboxStartButton = document.getElementById("flexbox-start-button");
-    ctx = myCanvas.getContext("2d");
-    resizeCanvas();
+    declareVariables();
+    resizeScreen();
+    eventListeners();
     setInterval(makeThemMove,25);
 }
 
-function resizeCanvas() {
+function declareVariables() {
+    myCanvas = document.getElementById("canvas");
+    ctx = myCanvas.getContext("2d");
+    myOverlay = document.getElementById("overlay");
+    myFlexboxControls = document.getElementById("flexbox-controls");
+    myFlexboxStartButton = document.getElementById("flexbox-start-button");
+    bubbles = [];
+}
+
+function resizeScreen() {
     myCanvas.width = window.innerWidth;
     myCanvas.height = window.innerHeight;
     myFlexboxControls.style.height = window.innerHeight + "px";
     myFlexboxStartButton.style.width = window.innerWidth + "px";
 }
 
-function ball() {
-    this.originalRadius = Math.random() * 35 + 25;
-    this.radius = this.originalRadius;
-    this.x = (Math.random() * (window.innerWidth - 100)) + 50;
-    this.y = (Math.random() * (window.innerHeight - 100)) + 50;
-    this.dirX = Math.round(Math.random() * 10) + 0.5;
-    this.dirY = Math.round(Math.random() * 10) + 0.5;
-    this.opa = Math.random() * 0.5 + 0.5;
-    this.opaRef1 = 0.55;
-    this.opaRef2 = 0.25;   
-    this.bubbleColor = "rgba(187, 253, 255, " + this.opa + ")";
-    this.largeReflectionColor = "rgba(255, 255, 255, " + this.opaRef1 + ")";
-    this.smallReflectionColor = "rgba(255, 255, 255, " + this.opaRef2 + ")";
-    this.popped = false;
-    this.update = function() {
-        ctx.beginPath();
-        ctx.arc(this.x,this.y,this.radius,0,Math.PI*2,true);
-        ctx.fillStyle = this.bubbleColor;
-        ctx.closePath();
-        ctx.fill();
-        ctx.beginPath();
-        ctx.arc(this.x - (0.4 * this.radius) , this.y - (0.4 * this.radius) , 0.3 * this.radius  , 0 , Math.PI*2 , true);
-        ctx.fillStyle = this.largeReflectionColor;
-        ctx.closePath();
-        ctx.fill();
-        ctx.beginPath();
-        ctx.arc(this.x + (0.5 * this.radius) , this.y + (0.5 * this.radius) , 0.1 * this.radius  , 0 , Math.PI*2 , true);
-        ctx.fillStyle = this.smallReflectionColor;
-        ctx.closePath();
-        ctx.fill();
-    }
+function eventListeners() {
+    window.addEventListener('resize', resizeScreen);
+    window.addEventListener('mousedown', detectButton);
+    window.addEventListener('mouseup', releaseBubble);
 }
 
-var balls = [];
-
-document.getElementById("start-button-itself").onclick = function() {
-    balls.push(new ball())
-}
-
-function makeThemMove() {
-    resizeCanvas();
-    ctx.clearRect(0,0,myCanvas.width,myCanvas.height);
-    for (var i = 0; i < balls.length; i++) {
-        if (balls[i].radius < 1.4 * balls[i].originalRadius) {
-            if (balls[i].popped == true) {
-                balls[i].radius *= 1.15;
-            }
-            balls[i].update();
-            if (balls[i].x < balls[i].radius || balls[i].x > myCanvas.width - balls[i].radius) {
-                balls[i].dirX = -balls[i].dirX;
-            }
-            if (balls[i].y < balls[i].radius || balls[i].y > myCanvas.height - balls[i].radius) {
-                balls[i].dirY = -balls[i].dirY;
-            }
-            if (balls[i].x > myCanvas.width) {
-                balls[i].x = myCanvas.width - 150;
-            }
-            if (balls[i].y > myCanvas.height) {
-                balls[i].y = myCanvas.height - 150;
-            }
-            balls[i].x += balls[i].dirX;
-            balls[i].y += balls[i].dirY;
-        }
+function detectButton(event) {
+    switch (event.button) {
+        case 0:
+            deleteBubble(event);
+            break;
+        case 2:
+            blowBubble(event);
+            break;
     }
 }
 
 function deleteBubble(event) {
     var mouseX = event.clientX;
     var mouseY = event.clientY;
-    for (var i = 0; i < balls.length; i++) {
-        var topX = balls[i].x - balls[i].radius;
-        var botX = balls[i].x + balls[i].radius;
-        var leftY = balls[i].y - balls[i].radius;
-        var rightY = balls[i].y + balls[i].radius;
+    for (var i = 0; i < bubbles.length; i++) {
+        var topX = bubbles[i].x - bubbles[i].radius;
+        var botX = bubbles[i].x + bubbles[i].radius;
+        var leftY = bubbles[i].y - bubbles[i].radius;
+        var rightY = bubbles[i].y + bubbles[i].radius;
         if (mouseX > topX && mouseX < botX && mouseY > leftY && mouseY < rightY) {
-            balls[i].popped = true;
+            bubbles[i].popped = true;
         }
     }
+}
+
+function blowBubble(event) {
+    var mouseX = event.clientX;
+    var mouseY = event.clientY;
+    bubbles.push(new makeBubble(mouseX, mouseY));
+}
+
+function makeBubble(x, y) {
+    this.originalRadius = 25;
+    this.radius = this.originalRadius;
+    this.x = x;
+    this.y = y;
+    this.dirX = 0;
+    this.dirY = 0;
+    this.opa = Math.random() * 0.5 + 0.5;
+    this.bubbleColor = "rgba(187, 253, 255, " + this.opa + ")";
+    this.largeReflectionColor = "rgba(255, 255, 255, 0.55)";
+    this.smallReflectionColor = "rgba(255, 255, 255, 0.25)";
+    this.popped = false;
+    this.ready = false;
+}
+
+function releaseBubble(event) {
+    if (event.button == 2) {
+        var n = bubbles.length;
+        if (bubbles[n-1].popped == false) {
+            bubbles[n-1].ready = true;
+            bubbles[n-1].dirX = Math.round(Math.random() * 20) -10;
+            bubbles[n-1].dirY = Math.round(Math.random() * 20) -10;
+            bubbles[n-1].originalRadius = bubbles[n-1].radius;
+        }
+
+    }
+}
+
+function makeThemMove() {
+    ctx.clearRect(0,0,myCanvas.width,myCanvas.height);
+    for (var i = 0; i < bubbles.length; i++) {
+        if (bubbles[i].ready == false && bubbles[i].radius > 70) {
+            bubbles[i].originalRadius = 70;
+            bubbles[i].popped = true;
+        }
+        if (bubbles[i].radius < 1.4 * bubbles[i].originalRadius || bubbles[i].popped == false) {
+            if (bubbles[i].popped == true) {
+                bubbles[i].radius *= 1.15;
+            }
+            if (bubbles[i].ready == false) {
+                bubbles[i].radius *= 1.02;
+            }
+            drawBubble(i);
+            updateBubble(i);
+        }
+    }
+}
+
+function drawBubble(i) {
+    drawCircle(bubbles[i].x, bubbles[i].y, bubbles[i].radius, bubbles[i].bubbleColor);
+    drawCircle(bubbles[i].x - (0.4 * bubbles[i].radius), bubbles[i].y - (0.4 * bubbles[i].radius), 0.3 * bubbles[i].radius, bubbles[i].largeReflectionColor);
+    drawCircle(bubbles[i].x + (0.5 * bubbles[i].radius), bubbles[i].y + (0.5 * bubbles[i].radius), 0.1 * bubbles[i].radius, bubbles[i].smallReflectionColor);
+}
+
+function drawCircle(x, y, radius, color) {
+    ctx.beginPath();
+    ctx.arc(x,y,radius,0,Math.PI*2,true);
+    ctx.fillStyle = color;
+    ctx.closePath();
+    ctx.fill();
+}
+
+function updateBubble(i) {
+    if (bubbles[i].x < bubbles[i].radius || bubbles[i].x > myCanvas.width - bubbles[i].radius) {
+        bubbles[i].dirX = -bubbles[i].dirX;
+    }
+    if (bubbles[i].y < bubbles[i].radius || bubbles[i].y > myCanvas.height - bubbles[i].radius) {
+        bubbles[i].dirY = -bubbles[i].dirY;
+    }
+    if (bubbles[i].x > myCanvas.width) {
+        bubbles[i].x = myCanvas.width - 150;
+    }
+    if (bubbles[i].y > myCanvas.height) {
+        bubbles[i].y = myCanvas.height - 150;
+    }
+    bubbles[i].x += bubbles[i].dirX;
+    bubbles[i].y += bubbles[i].dirY;
 }
